8a14d3ab14677d7218d7131ee5ee3ae8cb2a8dd3
nhatanh2103 <nguyennhatanh2103@gmail.com>
2024-02-21 23:27:40 +0700

    Update sql

--- profolio/database.js
+++ profolio/database.js
@@ -2,7 +2,7 @@ const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
 // Đường dẫn tới tệp cơ sở dữ liệu SQLite3
-const dbPath = "instance/database.db"
+const dbPath = "./instance/database.db"
 
 let db = new sqlite3.Database(dbPath, error => {
     if (error) {
